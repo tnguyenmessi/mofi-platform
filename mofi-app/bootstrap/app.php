3f1fff23825af52a5c95d26aa0da06b30de5878f
Nguyen Trung Tai Nguyen <tnguyenmessi@users.noreply.github.com>
2026-09-17 12:30:18 +0700

    fix: configure API guest response

--- mofi-app/bootstrap/app.php
+++ mofi-app/bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->redirectGuestsTo(fn (Request $request) => $request->is('api/*') ? null : '/login');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
